f74ecc92cdbe311fbcc800064e680d90df11bc06
Grace <gracemarsh27@gmail.com>
2022-01-14 22:57:46 +1100

    fix javascript add logo

--- js/main.js
+++ js/main.js
@@ -3,18 +3,34 @@
 // Get the button
 let scrollToTopButton = document.getElementById("btn-back-to-top");
 
+function scrollFunction() {
+  const scrolled = "scrolled";
+  const threshold = 20;
+  const body = document.body;
+  const hasScrolledClass = body.classList.contains(scrolled);
+  const isScrolled =
+    body.scrollTop > threshold ||
+    document.documentElement.scrollTop > threshold;
+
+  if (isScrolled && !hasScrolledClass) {
+    body.classList.add(scrolled);
+    scrollToTopButton.style.display = "block";
+    return;
+  }
+
+  if (!isScrolled && hasScrolledClass) {
+    scrollToTopButton.style.display = "none";
+    body.classList.remove(scrolled);
+  }
+}
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
+// check scroll position on initial page script in case the page is already scrolled.
+scrollFunction();
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollToTopButton.style.display = "block";
-  } else {
-    scrollToTopButton.style.display = "none";
-  }
-}
 // When the user clicks on the button, scroll to the top of the document
 scrollToTopButton.addEventListener("click", backToTop);
 
