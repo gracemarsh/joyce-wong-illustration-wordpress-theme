55841e36a66be556206665119b46a854438171f2
Grace <gracemarsh27@gmail.com>
2022-10-01 23:47:44 +1000

    remove duplicate no-title template variant

--- page-no-title.php
+++ page-no-title.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * The template for displaying all pages
  * Template Name: No Title
+ * The template for displaying all pages
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
